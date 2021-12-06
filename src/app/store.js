aa6cfcdc75bbbda90238d312f6d88147c4d1ca01
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-05 22:39:45 -0500

    add redux

---
+++ src/app/store.js
@@ -0,0 +1,7 @@
+// The state of the entire app is located here
+import { configureStore } from '@reduxjs/toolkit';
+
+export default configureStore({
+    reducer: {},
+});
+
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
 import 'antd/dist/antd.css';
 
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+// Redux
+import store from './app/store';
 
 ReactDOM.render(
   <Router>
+    <Provider store={store}>
     <App />
+    </Provider>
   </Router>,
   document.getElementById('root')
 );
