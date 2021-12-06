e3db0bb0fa04e90fef17c92b0d4a62ee54eb1edc
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-05 21:36:55 -0500

    destructure title from typography

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,12 +3,15 @@ import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
+// destructure Title from Typography antd
+const { Title } = Typography;
+
 function HomePage() {
     return (
         <>
-          <Typography.Title>
-              
-          </Typography.Title>
+          <Title>
+
+          </Title>
         </>
     )
 }
