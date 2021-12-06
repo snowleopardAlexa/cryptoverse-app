96aa5a72f455b2c29f407a8083c579ad92ed40fc
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-06 10:06:25 -0500

    update cryptoapi

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -5,27 +5,18 @@ const cryptoApiHeaders = {
   'x-rapidapi-host': 'coinranking1.p.rapidapi.com'
 };
 
-const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders})
 
 export const cryptoApi = createApi({
   reducerPath: 'cryptoApi',
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_CRYPTO_API_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
-    // endpoint
     getCryptos: builder.query({
-      query: (count) => createRequest(`/coins?limit=${count}`),
-    }),
-    getExchanges: builder.query({
-      query: () => createRequest('/exchanges'),
-    }),
-    getCryptoDetails: builder.query({
-      query: (coinId) => createRequest(`/coin/${coinId}`),
-    }),
-    getCryptoHistory: builder.query({
-      query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history/${timeperiod}`),
-    }),
-  }),
+      query: () => createRequest('/coins')
+    })
+  })
 });
 
-// redux toolkit hooks
-export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetExchangesQuery, useGetCryptoHistoryQuery } = cryptoApi;
+export const { useGetCryptosQuery } = cryptoApi;
