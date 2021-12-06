730c2a880f2dfbe25064298bba956b912bef5b2b
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-05 21:36:02 -0500

    import antd to homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
     return (
-        <div>
-            
-        </div>
+        <>
+          <Typography.Title>
+              
+          </Typography.Title>
+        </>
     )
 }
 
