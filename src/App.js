9a2475d57f046fc023b398103e003cd9f51ce5f2
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-05 21:06:42 -0500

    add routes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,27 @@ function App() {
            <Navbar />
         </div>
         <div className="main">
-
+            <Layout>
+              <div className="routes">
+                <Switch>
+                  <Route exact path="/">
+                      <HomePage />
+                  </Route>
+                  <Route exact path="/exchanges">
+                      <Exchanges />
+                  </Route>
+                  <Route exact path="/cryptocurrencies">
+                      <Cryptocurrencies />
+                  </Route>
+                  <Route exact path="/crypto/:coinId">
+                      <CryptoDetails />
+                  </Route>
+                  <Route exact path="/news">
+                      <News />
+                  </Route>
+                </Switch>
+              </div>
+            </Layout>
         </div>
         <div className="footer">
 
