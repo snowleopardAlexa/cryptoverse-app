58d9a08a51f6462a4d1fb7ccb16d93ec588ba245
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-05 20:24:08 -0500

    add skeleton app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
 
 function App() {
   return (
     <div className="cryptoverse-app">
-        
+        <div className="navbar">
+
+        </div>
+        <div className="main">
+
+        </div>
+        <div className="footer">
+
+        </div>
     </div>
   );
 }
