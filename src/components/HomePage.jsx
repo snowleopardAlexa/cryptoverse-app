3502fb86d8b09b77f74022691aa9d6f74916cfee
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-06 20:34:00 -0500

    add link to show more and title

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -17,17 +17,21 @@ function HomePage() {
 
     return (
         <>
-          <Title>
              <Title level={2} className="heading">Global Crypto Stats</Title>
              <Row>
-             <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
-        <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
-        <Col span={12}><Statistic title="Total Market Cap:" value={millify(globalStats.totalMarketCap)} /></Col>
-        <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} /></Col>
-        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
-        <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
+               <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+               <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+               <Col span={12}><Statistic title="Total Market Cap:" value={millify(globalStats.totalMarketCap)} /></Col>
+               <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} /></Col>
+               <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+               <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
              </Row>
-          </Title>
+             <div className="home-heading-container">
+                 <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
+                 <Title level={3} className="show-more">
+                     <Link to="/cryptocurrencies">Show More</Link>
+                 </Title>
+             </div>
         </>
     )
 }
