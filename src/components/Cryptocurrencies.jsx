489f1326b078efbadf49228ad6de5e6706696339
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-06 20:46:08 -0500

    add state cryptocurrencies

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import millify from 'millify';
+import { Link } from 'react-router-dom';
+import { Card, Row, Col, Input } from 'antd';
+
+import { useGetCryptosQuery } from '../services/cryptoApi';
 
 function Cryptocurrencies() {
+
+    const { data: cryptoList, isFetching } = useGetCryptosQuery();
+    const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+
     return (
         <div>
             
