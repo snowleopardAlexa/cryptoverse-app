909cdccbe28111dc88f7fbc80bd80f5e1e007b24
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-08 21:50:52 -0500

    add count 10 first cryptocurrencies

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
@@ -6,14 +6,15 @@ import { Card, Row, Col, Input } from 'antd';
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
 function Cryptocurrencies() {
-
-    const { data: cryptoList, isFetching } = useGetCryptosQuery();
+     
+    const count = simplified ? 10 : 100;
+    const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
     const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
 
     return (
         <div>
           <>
-            <Row gutters={[32, 32]} className="crypto-card-container">
+            <Row gutter={[32, 32]} className="crypto-card-container">
                   {cryptos.map((currency) => (
                       <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
                           <Link to={`/crypto/${currency.id}`}>
