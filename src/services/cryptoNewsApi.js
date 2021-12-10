52a35c1a25c50cccec02ba745088f7fb247619c9
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-09 21:08:14 -0500

    add crypto news headers from rapidapi

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,1 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeader = {
+        'x-bingapis-sdk': 'true',
+        'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+        'x-rapidapi-key': '864e50c568msh9db2cc8bb0fd09ap1670cejsn569399169fa5'
+}
