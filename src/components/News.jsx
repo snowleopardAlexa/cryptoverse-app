682b0857a6f96305120cce1c3a12ed08e4f1cb54
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-09 21:22:42 -0500

    destructure import antd, create const news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,6 +3,16 @@ import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
 import moment from 'moment';
 
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+import { useGetCryptosQuery } from '../services/cryptoApi';
+
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+const News = ({ simplified }) => {
+    const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency '})
+}
+
+console.log(cryptoNews);
 
 function News() {
     return (
