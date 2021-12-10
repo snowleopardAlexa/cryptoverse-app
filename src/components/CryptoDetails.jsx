c6524466c15ade7ccab322a51fa458b3b2c34eb5
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-10 18:31:14 -0500

    add statistics cryptodetails

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -38,9 +38,44 @@ function CryptoDetails() {
     ];
 
     return (
-        <div>
-            CryptoDetails {coinId}
-        </div>
+       <Col className="coin-detail-container">
+           <Col className="coin-heading-container">
+               <Title level={2} className="coin-name">
+                   {cryptoDetails.name} ({cryptoDetails.slug}) Price
+               </Title>
+               <p>
+                   {cryptoDetails.name} live price is US dollars.
+                   View value statistics, market cap and supply. 
+               </p>
+           </Col>
+           <Select 
+             defaultValue="7d" 
+             className="select-timeperiod" 
+             placeholder="Select Time Period"
+             onChange={(value) => setTimePeriod(value)}
+             >
+             {time.map((date) => <Option key={date}>{date}</Option>)}
+            </Select>
+
+            <Col className="stats-container">
+                <Col className="coin-value-statistics">
+                    <Col className="coin-value-statistics-heading">
+                        <Title level={3} className="coin-details-heading">{cryptoDetails.name} Value Statistics</Title>
+                        <p>An overview showing the statistics of {cryptoDetails.name}, such as the base and quote currency, the rank, and trading volume.</p>
+                    </Col>
+                    {stats.map(({ icon, title, value }) => (
+                        <Col className="coin-stats">
+                            <Col className="coin-stats-name">
+                               <Text>{icon}</Text>
+                               <Text>{title}</Text>
+                            </Col>
+                            <Text className="stats">{value}</Text>
+                        </Col>
+                    ))}
+                </Col>
+            </Col>
+
+       </Col>
     )
 }
 
