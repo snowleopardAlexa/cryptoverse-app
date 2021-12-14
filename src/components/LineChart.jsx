a20c81124fce2125833af4327f34057cbadb0d54
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-13 20:26:14 -0500

    add loader

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 import { Col, Row, Typography } from 'antd';
@@ -8,23 +9,26 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
   const coinPrice = [];
   const coinTimestamp = [];
 
-  for(let i = 0; i < coinHistory?.dat?.history?.length; i += 1) {
-    coinPrice.push(coinHistory.data.history[i].price)
-    coinTimestamp.push(new Date(coinHistory.data.history[i].timestamp).toLocaleDateString());
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinPrice.push(coinHistory?.data?.history[i].price);
+  }
+
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString());
   }
 
   const data = {
     labels: coinTimestamp,
     datasets: [
       {
-        label: 'Price in USD',
+        label: 'Price In USD',
         data: coinPrice,
         fill: false,
         backgroundColor: '#0071bd',
         borderColor: '#0071bd',
-      }
-    ]
-  }
+      },
+    ],
+  };
 
   const options = {
     scales: {
@@ -32,11 +36,11 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
         {
           ticks: {
             beginAtZero: true,
-          }
-        }
-      ]
-    }
-  }
+          },
+        },
+      ],
+    },
+  };
 
   return (
     <>
