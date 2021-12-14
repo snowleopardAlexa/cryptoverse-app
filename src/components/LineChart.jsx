c9381da93bfa0c383783545d7c850e8e19093eb7
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-13 20:34:44 -0500

    reinstall versions of chart.js and react-chartjs-2 one down due to a bug

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,6 +1,6 @@
-
 import React from 'react';
 import { Line } from 'react-chartjs-2';
+import {CategoryScale} from 'chart.js';
 import { Col, Row, Typography } from 'antd';
 
 const { Title } = Typography;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
-import { HomeOutlined, FundOutlined, MoneyCollectOutlined, BulbOutlined, MenuOutlined } from '@ant-design/icons';
+import { HomeOutlined, FundOutlined, MoneyCollectOutlined, BulbOutlined } from '@ant-design/icons';
 
 import icon from '../img/cryptocurrency.png';
 
