2c42bf139a84dd182b3d1c485191a62ac46c4b6f
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-15 20:12:39 -0500

    add skeleton exchanges

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -2,10 +2,38 @@ import React from 'react';
 
 function Exchanges() {
     return (
-        <div>
-            
-        </div>
-    )
+        <>
+          <Row>
+              <Col span={6}>Exchanges</Col>
+              <Col span={6}>24th Trade Volume</Col>
+              <Col span={6}>Markets</Col>
+              <Col span={6}>Change</Col>
+          </Row>
+          <Row>
+              <Col span={24}>
+                  <Collapse>
+                    <Panel
+                      key={exchange.id}
+                      showArrow={false}
+                      header={(
+                          <Row key={exchange.id}>
+                              <Col span={6}>
+                                  <Text><strong></strong></Text>
+                                  <Avatar className="exchange-image" />
+                                  <Text><strong></strong></Text>
+                              </Col>
+                              <Col span={6}></Col>
+                              <Col span={6}></Col>
+                              <Col span={6}></Col>
+                            </Row>  
+                          )}
+                       >
+                       </Panel>    
+                  </Collapse>
+              </Col>
+          </Row>
+        </>
+    );
 }
 
 export default Exchanges;
